01d40b36d4240b92dd651e74056418d25c947072
Ana <ana.smith@itglobers.com>
2023-02-19 22:25:29 -0500

    feat: add sku validation

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -5,7 +5,6 @@ import GET_PRODUCT from '../graphql/getProductBySku.graphql'
 import { useCssHandles } from 'vtex.css-handles'
 
 const QuickOrder = () => {
-
   const CSS_HANDLES = [
     "contaner_quick-order",
     "text_quick-order",
@@ -22,6 +21,7 @@ const QuickOrder = () => {
     text: "",
     quantity: ""
   });
+  const [errorMessage, setErrorMessage] = useState(false)
 
   const [getProductData, { data: product }] = useLazyQuery(GET_PRODUCT)
   const [addToCart] = useMutation(UPDATE_CART);
@@ -32,18 +32,25 @@ const QuickOrder = () => {
       [e.target.id]: e.target.value
     })
   }
-
   useEffect(() => {
+    getProductData({
+      variables: {
+        sku: inputText.text
+      }
+    })
+  }, [inputText.text])
+
+  const searchProduct = (evt: any) => {
+    evt.preventDefault()
+    setErrorMessage(false)
     if (product) {
-      let skuId = parseInt(inputText.text)
-      let quantityBuy = parseInt(inputText.quantity)
       addToCart({
         variables: {
           salesChannel: "1",
           items: [
             {
-              id: skuId,
-              quantity: quantityBuy,
+              id: parseInt(inputText.text),
+              quantity: parseInt(inputText.quantity),
               seller: "1"
             }
           ]
@@ -52,59 +59,37 @@ const QuickOrder = () => {
         .then(() => {
           window.location.href = "/checkout"
         })
-    }
-  }, [product])
-
-  const addProductToCart = () => {
-    getProductData({
-      variables: {
-        sku: inputText.text
-      }
-    })
+    } else if (!product) setErrorMessage(true)
   }
-
-  const searchProduct = (e: any) => {
-    e.preventDefault();
-    if (!inputText.text) {
-      alert("Please enter a product name")
-    } else {
-      addProductToCart();
-    }
-  }
-
   return (
     <div className={handles['contaner_quick-order']}>
-      <h2 className={handles['text_quick-order']}>
-        Compra rapida
-      </h2>
+      <h2 className={handles['text_quick-order']}> Compra rapida </h2>
       <form onSubmit={searchProduct} className={handles['form_quick-order']}>
         <div className={handles['container_input-form']}>
-          <p className={handles['text_input-form']}>
-            Ingresa el número de SKU:
-          </p>
+          <p className={handles['text_input-form']}> Ingresa el número de SKU: </p>
           <input
-            type="text"
             id="text"
+            type="text"
             value={inputText.text}
             onChange={handleChange}
             className={handles['input-form']} />
         </div>
         <div className={handles['container_input-form']}>
-          <p className={handles['text_input-form']}>
-            Ingresa la cantidad:
-          </p>
+          <p className={handles['text_input-form']}> Ingresa la cantidad: </p>
           <input
-            type="number"
-            name="quantity"
-            value={inputText.quantity}
+            min="1"
+            max="20"
             id="quantity"
+            type="number"
+            value={inputText.quantity}
             onChange={handleChange}
             className={handles['input-form']} />
         </div>
+        {errorMessage && (<p>- Tu producto no se ha encontrado -</p>)}
         {
-          inputText.text && inputText.quantity && (
+          inputText.text && inputText.quantity ? (
             <input className={handles['button_submit']} type="submit" value="Añadir al carrito" />
-          )
+          ) : <input className={handles['button_submit']} disabled type="submit" value="Añadir al carrito" />
         }
       </form >
     </div >
